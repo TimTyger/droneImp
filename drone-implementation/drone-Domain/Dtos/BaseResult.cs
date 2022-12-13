672a13c909f64cb259379c1b13adf32bc873153d
Adebo Timothy <adebortimothy@gmail.com>
2022-12-13 03:12:26 +0100

    added skeleton read-write controller as dispatch Controller

--- drone-implementation/drone-Domain/Dtos/BaseResult.cs
+++ drone-implementation/drone-Domain/Dtos/BaseResult.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Domain.Dtos
+namespace drone_Domain.Dtos
 {
     public class BaseResult<T>
     {
--- drone-implementation/drone-implementation/Program.cs
+++ drone-implementation/drone-implementation/Program.cs
@@ -1,6 +1,19 @@
+using drone_DataAccess.Repositories;
+using drone_DataAccess.UnitOfWork;
+using drone_Domain.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+#region Repositories
+builder.Services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddTransient<IMedicationRepository, MedicationRepository>();
+builder.Services.AddTransient<IDroneRepository, DroneRepository>();
+builder.Services.AddTransient<IDroneModelRepository, DroneModelRepository>();
+builder.Services.AddTransient<IStateRepository, StateRepository>();
+#endregion
+builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +28,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors(builder =>
+{
+    builder
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader();
+});
 
 app.UseHttpsRedirection();
 
