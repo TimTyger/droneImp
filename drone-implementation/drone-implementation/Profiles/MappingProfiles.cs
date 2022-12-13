59c48ba7c00ff87a76b93c7ea89d823babb7de35
Adebo Timothy <adebortimothy@gmail.com>
2022-12-13 17:24:18 +0100

    added automapper and git ignore

---
+++ drone-implementation/drone-implementation/Profiles/MappingProfiles.cs
@@ -0,0 +1,14 @@
+﻿using drone_Domain.Dtos;
+using drone_Domain.Entities;
+using AutoMapper;
+
+namespace drone_implementation.Profiles
+{
+    public class MappingProfiles:Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Drone, RegisterDroneDto>().ReverseMap();
+        }
+    }
+}
